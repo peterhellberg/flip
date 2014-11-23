c7fb511bf1b9bca03d71c166675657c55ec09f29
Peter Hellberg <peter@c7.se>
2014-11-23 01:08:31 +0100

    Simple command line client for the flip library

---
+++ cmd/flip/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/peterhellberg/flip"
+)
+
+func main() {
+	fmt.Println(flipString(os.Args[1:]))
+}
+
+func flipString(args []string) string {
+	if len(args) > 0 {
+		if args[0] == "table" && len(args) > 1 {
+			return flip.Table(strings.Join(args[1:], " "))
+		}
+
+		return flip.UpsideDown(strings.Join(args, " "))
+	}
+
+	return ""
+}
