af58f5de8f6eb4b76acfdd0a3350a5ae86de513d
Peter Hellberg <peter@c7.se>
2015-01-08 19:26:41 +0100

    Command line application documentation

--- cmd/flip/main.go
+++ cmd/flip/main.go
@@ -1,3 +1,24 @@
+/*
+
+flip (╯°□°）╯︵ʇxǝʇ
+
+A command line client used to flip text.
+
+Installation
+
+Just go get the command:
+
+    go get -u github.com/peterhellberg/flip/cmd/flip
+
+Usage
+
+You can flip a string on its own or decorate it with a named emoji
+
+    $ flip table foo  #=> (╯°□°）╯︵ooɟ
+    $ flip gopher bar #=> ʕ╯◔ϖ◔ʔ╯︵ɹɐq
+    $ flip baz        #=> zɐq
+
+*/
 package main
 
 import (
